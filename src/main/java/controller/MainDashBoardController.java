2c01aecec1c28a8b9d7a87ce2255777e6bb95248
INDUWARA90 <induwaravishwakantha90@gmail.com>
2024-10-07 21:28:43 +0530

    Add Main Portal

---
+++ src/main/java/controller/MainDashBoardController.java
@@ -0,0 +1,18 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class MainDashBoardController {
+
+    @FXML
+    void AdminPortalOnActionBtn(ActionEvent event) {
+
+    }
+
+    @FXML
+    void EmployeePortalOnActionBtn(ActionEvent event) {
+
+    }
+
+}
