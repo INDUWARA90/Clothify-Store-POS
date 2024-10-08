3f15a69637c984930f00ca7f7c5583af037ee9c9
INDUWARA90 <induwaravishwakantha90@gmail.com>
2024-10-08 21:38:50 +0530

    Employee DashBoard UI

---
+++ src/main/java/controller/admin/LoginAdminDashBoard.java
@@ -0,0 +1,28 @@
+package controller.admin;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class LoginAdminDashBoard {
+
+    @FXML
+    void btnCustomerManageOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnGenarateReportsOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnPOSSystemOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btnProductsManageOnAction(ActionEvent event) {
+
+    }
+
+}
